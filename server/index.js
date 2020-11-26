354e16960851a3691de45518655e7f862051ded0
Ruimas-UTI <allpraisethelord@gmail.com>
2020-11-26 19:19:36 +0600

    initial server setup

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
+
+app.use("/posts", postRoutes);
+
 
 const CONNECTION_URL = "mongodb+srv://Memories:memories123@cluster0.8rxa2.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
