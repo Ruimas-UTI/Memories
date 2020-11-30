ecb6a7aaa14c5c79389f78c0d15f586a94c2913c
Ruimas-UTI <allpraisethelord@gmail.com>
2020-12-01 05:48:37 +0600

    post route fixed

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -6,12 +6,10 @@ import FileBase from "react-file-base64";
 import {createPost} from "../../actions/posts";
 
 export default function Form() {
-    const [postData, setpostData] = useState({
-        creator: " ", title:" ", message: " ", tags: " ", selectedFile: " "
-    });
+    const [postData, setpostData] = useState({creator: " ", title:" ", message: " ", tags: " ", selectedFile: " "});
     const dispatch = useDispatch();
     const classes = useStyles();
-    const handleSubmit = e =>{
+    const handleSubmit = e =>{ 
         e.preventDefault();
         // console.log(postData);
         dispatch(createPost(postData))
@@ -23,10 +21,10 @@ export default function Form() {
        <Paper className={classes.paper}>
            <form autoComplete="off" noValidate onSubmit={handleSubmit} className={`${classes.root} ${classes.form}`}>
                 <Typography variant="h6">Creating A Memory</Typography>
-                <TextField name="creator" label="Creator"variant="outlined" fullWidth value={postData.creator}onChange={ e => setpostData({...postData, creator: e.target.value})}/>
-                <TextField name="title" label="Title"variant="outlined" fullWidth value={postData.title}onChange={ e => setpostData({...postData, title: e.target.value})}/>
-                <TextField name="message" label="Message"variant="outlined" fullWidth value={postData.message}onChange={ e => setpostData({...postData, message: e.target.value})}/>
-                <TextField name="tags" label="Tags"variant="outlined" fullWidth value={postData.tags}onChange={ e => setpostData({...postData, tags: e.target.value})}/>
+                <TextField name="creator" label="Creator"variant="outlined" fullWidth value={postData.creator} onChange={ e => setpostData({...postData, creator: e.target.value})}/>
+                <TextField name="title" label="Title"variant="outlined" fullWidth value={postData.title} onChange={ e => setpostData({...postData, title: e.target.value})}/>
+                <TextField name="message" label="Message"variant="outlined" fullWidth value={postData.message} onChange={ e => setpostData({...postData, message: e.target.value})}/>
+                <TextField name="tags" label="Tags"variant="outlined" fullWidth value={postData.tags} onChange={ e => setpostData({...postData, tags: e.target.value})}/>
                 <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setpostData({...postData, selectedFile: base64})}/></div>
                 <Button className={classes.buttonSubmit} color="primary" variant="contained" type="submit" size="large">Submit</Button>           
                 <Button color="secondary" variant="contained" onClick={clear} size="small" fullWidth>Clear</Button>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import { Mongoose } from "mongoose";
 import postMessages from "../models/postMessages.js";
 
 export const getPosts = async (req,res) => {
@@ -17,4 +18,16 @@ export const createPosts = async(req, res) => {
     } catch (error) {
         res.status(409).json({message: error.message})
     }
+}
+export const updatePost = async (req, res) => {
+    const {id: _id} = req.parms;
+    const post = req.body;
+    if(! Mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that ID");
+    const updatedPost = await postMessages.findByIdAndUpdate(_id, post, { new: true });
+    res.json(updatedPost);
+}
+export const deletePost = async (req,res) => {
+    const {id: _id} = req.params;
+    if(! Mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("Cannot delete because ID is invalid");
+    postMessages.findByIdAndDelete(_id);
 }
